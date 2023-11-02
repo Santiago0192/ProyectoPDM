1de3407399c858352dc8aa1ce7ddc1de767a5080
Diegordo17 <diegosolorzano608@gmail.com>
2023-11-01 23:31:06 -0600

    Ventana de historial

---
+++ app_finanzas/lib/data/constrants.dart
@@ -0,0 +1,67 @@
+const String historial_transacciones = '''
+[
+  {
+    "tienda": "Walmart",
+    "fecha": "2023-10-31",
+    "monto": 2000,
+    "argumento": "Compras de supermercado",
+    "tipo": "Ingreso"
+  },
+  {
+    "tienda": "Soriana",
+    "fecha": "2023-10-30",
+    "monto": 1500,
+    "argumento": "Pago de servicios",
+    "tipo": "Gasto"
+  },
+  {
+    "tienda": "Liverpool",
+    "fecha": "2023-10-29",
+    "monto": 5000,
+    "argumento": "Compra de ropa",
+    "tipo": "Ingreso"
+  },
+  {
+    "tienda": "Coppel",
+    "fecha": "2023-10-28",
+    "monto": 3000,
+    "argumento": "Compra de electrónicos",
+    "tipo": "Gasto"
+  },
+  {
+    "tienda": "Oxxo",
+    "fecha": "2023-10-27",
+    "monto": 200,
+    "argumento": "Compra de snacks",
+    "tipo": "Ingreso"
+  },
+  {
+    "tienda": "Starbucks",
+    "fecha": "2023-10-26",
+    "monto": 100,
+    "argumento": "Café",
+    "tipo": "Ingreso"
+  },
+  {
+    "tienda": "Netflix",
+    "fecha": "2023-10-25",
+    "monto": 200,
+    "argumento": "Pago de suscripción",
+    "tipo": "Gasto"
+  },
+  {
+    "tienda": "Spotify",
+    "fecha": "2023-10-24",
+    "monto": 100,
+    "argumento": "Pago de suscripción",
+    "tipo": "Gasto"
+  },
+  {
+    "tienda": "Uber",
+    "fecha": "2023-10-23",
+    "monto": 500,
+    "argumento": "Viaje al trabajo",
+    "tipo": "Gasto"
+  }
+]
+''';
--- app_finanzas/lib/screens/menu/historial.dart
+++ app_finanzas/lib/screens/menu/historial.dart
@@ -1,15 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:app_finanzas/funciones/ClickContainer.dart';
+//import '../../data/historial.json';
+import 'dart:convert';
+import 'package:app_finanzas/data/constrants.dart';
+
+
+
 
 class historial extends StatefulWidget {
   const historial({super.key});
-
   @override
   State<historial> createState() => _historialState();
 }
 
 class _historialState extends State<historial> {
   @override
+  List<dynamic> historial = [];
+  
+
+  void initState(){
+    historial = jsonDecode(historial_transacciones);
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: (
+        HistorialList(historial: historial)
+      )
+    );
   }
 }
+
+class HistorialList extends StatelessWidget{
+  const HistorialList({super.key, required this.historial});
+
+  final List historial;
+  @override
+  Widget build(BuildContext context){
+    return Stack(
+      children: [
+Positioned(
+            top: 140.0,
+            left: -250,
+            right: 0,
+            child: Center(
+              child: Column(
+                children : [
+                  Text(
+                'Egresos',
+                style: TextStyle(
+                  fontSize: 36.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+
+                
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 70),
+                child: Container(
+                  height: 10,
+                  width: 200,
+                  color: Color.fromARGB(255, 226, 33, 243),
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                'Ingresos',
+                style: TextStyle(
+                  fontSize: 36.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+                
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left: 170),
+                child: Container(
+                  height: 10,
+                  width: 300,
+                  color: Color.fromARGB(255, 84, 183, 201),
+                ),
+              )
+              ]
+              )
+            ),
+          ),
+
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: MediaQuery.of(context).size.height * 1 / 2,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 241, 241, 241),
+              borderRadius: BorderRadius.vertical(
+              top: Radius.circular(30.0),
+              ),
+            ),
+
+            child: ListView.separated(
+              itemCount: historial.length,
+              separatorBuilder: (context, index){
+              return const Divider();
+              },
+
+              itemBuilder: (context, index){
+                return ListTile(
+                leading: Text(
+                  "       ${historial[index]['tipo']}",
+                  style: TextStyle(
+                    color: (historial[index]['tipo']) == "Gasto" ? Color.fromARGB(255, 226, 33, 243) : Color.fromARGB(255, 84, 183, 201),
+                  )),
+                title: Text('   ${historial[index]['tienda']}'),
+                subtitle: Text('   ${historial[index]['fecha']} \n   ${historial[index]['argumento']}'),
+                trailing: Text(
+                  "\$${historial[index]['monto']}.00          ",
+                  style: TextStyle(
+                    color: (historial[index]['tipo']) == "Gasto" ? Color.fromARGB(255, 226, 33, 243) : Color.fromARGB(255, 84, 183, 201),
+                  )),
+                
+               );
+             }
+            )
+          )
+        ),
+      ]
+    );
+  }
+}
+
+
+        
